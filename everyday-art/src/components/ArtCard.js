cc590f1972a897dbbf169658822a1c6e2f0e9fd0
Molly Graham <mollyclaire.graham@gmail.com>
2019-09-10 16:05:19 -0500

    Data now renders with ArtCard Component and Main Component.

--- everyday-art/src/components/ArtCard.js
+++ everyday-art/src/components/ArtCard.js
@@ -1,15 +1,29 @@
 import React from "react";
 
 function ArtCard(props) {
+  return(
+  props.artworks.map(artwork => {
     return (
       <div className="text-center">
         {/* <img alt={props.title} className="img-fluid" src={props.primaryImageSmall} style={{ margin: "0 auto" }} /> */}
-        <h3>Title: {props.title}</h3>
-        <h3>Medium: {props.medium}</h3>
+        <h3>Title: {artwork.title}</h3>
+        <h3>Medium: {artwork.medium}</h3>
         {/* <h3>Artist: {props.artists}</h3> */}
-        <h3>Description: {props.description}</h3>
-      </div>
-    );
+        <h3>Description: {artwork.description}</h3>
+      </div>)
+    
+  })
+  )
+  
+    // return (
+    //   <div className="text-center">
+    //     {/* <img alt={props.title} className="img-fluid" src={props.primaryImageSmall} style={{ margin: "0 auto" }} /> */}
+    //     <h3>Title: {props.title}</h3>
+    //     <h3>Medium: {props.medium}</h3>
+    //     {/* <h3>Artist: {props.artists}</h3> */}
+    //     <h3>Description: {props.description}</h3>
+    //   </div>
+    // );
   }
   
   export default ArtCard;
