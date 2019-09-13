f8af8140eb6adf99f54fbbf3ded6b39547f030bf
Molly Graham <mollyclaire.graham@gmail.com>
2019-09-13 14:35:28 -0500

    Updated Header with styles.

--- everyday-art/src/App.js
+++ everyday-art/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import SearchResult from "./components/SearchResult";
+// import SearchResult from "./components/SearchResult";
 import Container from "./components/Container";
 import Header from "./components/Header";
 import Main from "./pages/Main";
@@ -7,7 +7,7 @@ import Main from "./pages/Main";
 function App() {
   return (
     <Container>
-      <Header>Art for Today</Header>
+      <Header></Header>
       <Main />
     </Container>
   )
--- everyday-art/src/components/Header.js
+++ everyday-art/src/components/Header.js
@@ -1,28 +1,50 @@
 import React from "react"; 
 import styled from "styled-components";
+import icon from './calendar.png';
 
 const Art = styled.div`
-    font-size: 1.5em;
+    font-size: 2em;
     text-align: center;
-    color: palevioletred;
-    padding: 2em;
+    color: black;
+    padding: .5em;
     display: flex;
-    justify-content: center;
+    font-family: 'Montserrat', sans-serif
 `
 
-const Date = styled.div`
-  color: black;
-  text-align: center;
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid black;
+`
+
+const Date = styled.img`
+    width: 150px;
 `
 
 class Header extends React.Component  {
-    state = {}
+    
+    // state = {
+    //     date: ""
+    // }
+
+    // componentDidMount() {
+    //     this.getDate();
+    // }
+
+    // getDate = () => {
+    //     var date = new Date().getDate();
+    //     this.setState({date: date});
+    // }
 
     render() {
+        // const { date } = this.state;
         return(
             <div>
-                <Art>ART for</Art>
-                <Date>TODAY</Date>
+                <Container>
+                    <Art>ART for</Art>
+                    <Date src={icon} alt="Calendar"/>
+                </Container>
             </div>
   
         )
